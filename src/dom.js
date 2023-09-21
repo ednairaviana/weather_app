7b107ae18836d439adbe826890278ba54d83b1b4
Ednaira Viana <ednairaviana.evs@gmail.com>
2023-09-21 15:37:53 -0300

    Create reset.css

--- src/dom.js
+++ src/dom.js
@@ -2,7 +2,7 @@ import { dataBase } from "./database";
 import { updateClock } from "./clock";
 
 async function doThings() {
-  console.log(dataBase.location.tz_id); //display things -> wait for data
+  console.log(dataBase); //display things -> wait for data
 }
 
 export { doThings };
