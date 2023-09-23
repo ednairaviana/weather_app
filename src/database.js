d0a7e601b1924be3f1b7d4e96257e5e072b718f6
Ednaira Viana <ednairaviana.evs@gmail.com>
2023-09-23 17:36:48 -0300

    Fix procotocol of getData

--- src/database.js
+++ src/database.js
@@ -4,7 +4,7 @@ const main = document.querySelector("main");
 
 async function getData(value) {
   const response = await fetch(
-    `http://api.weatherapi.com/v1/forecast.json?key=c55ebf895da8496991f222042231809&q=${value}&days=10&aqi=no&alerts=no`
+    `https://api.weatherapi.com/v1/forecast.json?key=c55ebf895da8496991f222042231809&q=${value}&days=10&aqi=no&alerts=no`
   );
   const data = await response.json();
 
